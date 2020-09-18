f675a9e1e964b740264b0e5ba355fc3e100efc8a
oliveiraFab16 <fabriciocarvalho932@gmail.com>
2020-09-18 16:03:54 -0300

    repete

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string digiteUmaPalavra;
+
+            //<---Inicio(Digite)--->//
+            Console.Write("digiteUmaPalavra: ");
+            digiteUmaPalavra = Console.ReadLine();
+            Console.Write("\n");
+
+            Console.WriteLine($"{digiteUmaPalavra}!!!".ToUpper());
+            //<---Fim(Digite)--->//
+
+            //<---Inicio(Sair)--->//
+            Console.WriteLine("Pressione ENTER para sair");
+            Console.ReadKey();
+            //<---Fim(Sair)--->//
+        }
+    }
+}
